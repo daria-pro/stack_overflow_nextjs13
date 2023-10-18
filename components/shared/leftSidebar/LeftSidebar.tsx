56c4912884bba53f9d899bed0751e50a117c5935
daria-pro <shaulkod@gmail.com>
2023-10-18 22:42:56 +0300

    add right sidebar

--- components/shared/leftSidebar/LeftSidebar.tsx
+++ components/shared/leftSidebar/LeftSidebar.tsx
@@ -11,7 +11,7 @@ import { Button } from "@/components/ui/button";
 const LeftSidebar = () => {
   const pathname = usePathname();
   return (
-    <div className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 flex h-screen w-fit flex-col justify-between gap-6 overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
+    <section className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 flex h-screen w-fit flex-col justify-between gap-6 overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
       <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((item) => {
           const isActive =
@@ -61,7 +61,7 @@ const LeftSidebar = () => {
                 src="/assets/icons/sign-up.svg"
                 width={20}
                 height={20}
-                alt="login"
+                alt="sign up"
                 className="invert-colors lg:hidden"
               />
               <span className="max-lg:hidden">Sign Up</span>
@@ -69,7 +69,7 @@ const LeftSidebar = () => {
           </Link>
         </SignedOut>
       </div>
-    </div>
+    </section>
   );
 };
 
