789d237ff5dd5042f1bd491b402f46b59a9a57d1
daria-pro <shaulkod@gmail.com>
2023-11-04 15:12:42 +0200

    add question card component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -6,39 +6,40 @@ import Filter from "@/components/shared/Filter";
 import { HomePageFilters } from "@/constants/filters";
 import HomeFilters from "@/components/home/HomeFilters";
 import NoResult from "@/components/shared/NoResult";
+import QuestionCard from "@/components/cards/QuestionCard";
 
 const questions = [
   {
-    _id: 1,
+    _id: "1",
     title: "Cascading Deletes in SQLAlchemy?",
     tags: [
       {
-        _id: 1,
+        _id: "1",
         name: "python",
       },
-      { _id: 2, name: "sql" },
+      { _id: "2", name: "sql" },
     ],
-    author: "John Doe",
+    author: { _id: "1", name: "John Doe", picture: "john-doe.jpg" },
     upvotes: 10,
     views: 100,
-    answers: 2,
-    createdAt: "2021-09-01T12:00:00.000Z",
+    answers: [],
+    createdAt: new Date("2021-09-01T12:00:00.000Z"),
   },
   {
-    _id: 2,
+    _id: "2",
     title: "How to center a div?",
     tags: [
       {
-        _id: 1,
+        _id: "1",
         name: "css",
       },
-      { _id: 2, name: "html" },
+      { _id: "2", name: "html" },
     ],
-    author: "John Doe",
+    author: { _id: "1", name: "John Doe", picture: "john-doe.jpg" },
     upvotes: 10,
     views: 100,
-    answers: 2,
-    createdAt: "2021-09-01T12:00:00.000Z",
+    answers: [],
+    createdAt: new Date("2021-09-01T12:00:00.000Z"),
   },
 ];
 
@@ -70,8 +71,20 @@ export default function Home() {
 
       <HomeFilters />
       <div className="mt-10 flex w-full flex-col gap-6">
-        {!(questions.length > 0) ? (
-          questions.map((question) => "Question Card")
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
         ) : (
           <NoResult
             title="There's no question to show"
