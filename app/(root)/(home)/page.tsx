0109232d4a15f586e8aa44a442424869df596f39
daria-pro <shaulkod@gmail.com>
2023-11-04 14:43:59 +0200

    added questions constant

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -6,6 +6,41 @@ import Filter from "@/components/shared/Filter";
 import { HomePageFilters } from "@/constants/filters";
 import HomeFilters from "@/components/home/HomeFilters";
 
+const questions = [
+  {
+    _id: 1,
+    title: "Cascading Deletes in SQLAlchemy?",
+    tags: [
+      {
+        _id: 1,
+        name: "python",
+      },
+      { _id: 2, name: "sql" },
+    ],
+    author: "John Doe",
+    upvotes: 10,
+    views: 100,
+    answers: 2,
+    createdAt: "2021-09-01T12:00:00.000Z",
+  },
+  {
+    _id: 2,
+    title: "How to center a div?",
+    tags: [
+      {
+        _id: 1,
+        name: "css",
+      },
+      { _id: 2, name: "html" },
+    ],
+    author: "John Doe",
+    upvotes: 10,
+    views: 100,
+    answers: 2,
+    createdAt: "2021-09-01T12:00:00.000Z",
+  },
+];
+
 export default function Home() {
   return (
     <>
@@ -33,6 +68,7 @@ export default function Home() {
       </div>
 
       <HomeFilters />
+      <div className="mt-10 flex w-full flex-col gap-6"></div>
     </>
   );
 }
